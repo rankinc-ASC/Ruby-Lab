cc6eed252f6c489bf7a40e012435a16c93af4478
rankinc-ASC <rankinc@alfredstate.edu>
2018-12-02 01:41:25 -0500

    input field restrictions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,10 @@ class Restaurant < ApplicationRecord
 
     validates_presence_of :name, :description, :contactInfo, :location
 
+    validates_length_of :name, :maximum => 50
+    validates_length_of :description, :maximum => 2000
+    validates_length_of :contactInfo, :maximum => 200
+    validates_length_of :location, :maximum => 100
 
     def self.search(search)
         where("name LIKE ? OR location LIKE ?", "%#{search}%", "%#{search}%")
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :restaurant
     belongs_to :user
+
+    validates_presence_of :rating, :comment
+
+    validates_length_of :comment, :maximum => 200
 end
